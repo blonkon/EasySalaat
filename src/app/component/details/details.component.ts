937ef2f9e555958b4205dc40dbf006328619bc3b
ibrahim10sy <ibrahim10sy@gmail.com>
2023-09-09 21:17:52 +0100

    commit av sup

--- src/app/component/details/details.component.ts
+++ src/app/component/details/details.component.ts
@@ -10,50 +10,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent  implements OnInit {
-
-  id:any;
-  item : any;
-  constructor(private serviceSourate : SourateService, private route:ActivatedRoute) { }
-
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.id = params.get('id');
-      console.log(this.id);
-      if (this.id) {
-        this.getSourateById(this.id);
-      }
-    });
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-  // ngOnInit() {
-  //   this.route.paramMap.subscribe(params => {
-  //     const id = +params.get('id'); // Convertir en nombre
-  //     if (id) {
-  //       this.getSourateById(id);
-  //     }
-  //   });
-  
-  
-  getSourateById(id: number) {
-    this.serviceSourate.getSurahById(id).subscribe(surah => {
-      this.item = surah;
-      console.log(this.item);
-    });
-  }
+
  
-  
-  
-  
-  
-//   getSourateById(id:number){
-//   this.serviceSourate.getSurahById(id).subscribe(surah =>{
-//     this.item = surah;
-//     console.log(this.item);
-//   });
-// }
-// getSourateById(id:number){
-//   this.serviceSourate.getSurahById(id).subscribe(surah =>{
-//     this.item = surah.data
-//     console.log(this.item);
-//   });
-// }
 }
