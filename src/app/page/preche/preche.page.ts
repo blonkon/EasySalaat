f5b9f92b16ca1695c97d3b00f19f7c07f7c1104a
Mamadou <mohamedfdg72@gmail.com>
2023-09-09 18:49:33 +0000

    preche

--- src/app/page/preche/preche.page.ts
+++ src/app/page/preche/preche.page.ts
@@ -9,17 +9,31 @@ import { Media, MediaObject } from '@ionic-native/media/ngx'; // Importez le plu
 })
 export class PrechePage {
   playIcon = 'pause';
-  audioFile !: MediaObject;// Déclarez la variable pour gérer la lecture audio
+  audioFile!: MediaObject;// Déclarez la variable pour gérer la lecture audio
+  preachers = [
+    { name: 'Imam Fadiga', description: 'Le tout puissant', audioUrl: '../../assets/audio/preche1.mp3' },
+    { name: 'Imam Blonko', description: 'La croyance', audioUrl: '../../assets/audio/preche1.mp3' },
+    { name: 'Imam Mohamed Diarra', description: 'Le tout puissant', audioUrl: '../../assets/audio/preche1.mp3' },
+    { name: 'Imam Lacine Keïta', description: 'Le tout puissant', audioUrl: '../../assets/audio/preche1.mp3' },
+    { name: 'Imam Ibrahim Sy', description: 'Le tout puissant', audioUrl: '../../assets/audio/preche1.mp3' },
+    // Ajoutez d'autres prêcheurs ici
+  ];
+
 
   constructor(private media: Media) {
   }
 
   ngOnInit() {
     // Créez un objet audio pour votre fichier audio
-    this.audioFile = this.media.create('chemin/vers/votre-fichier-audio.mp3');
+    // this.audioFile = this.media.create('../../assets/audio/preche1.mp3');
   }
 
-  playPause() {
+  playPause(audioUrl: string) {
+    // Créez un objet audio pour le fichier audio correspondant au prêcheur en cours
+    if (!this.audioFile) {
+      this.audioFile = this.media.create(audioUrl);
+    }
+
     if (this.playIcon === 'pause') {
       this.playIcon = 'play';
       this.audioFile.pause();
@@ -28,4 +42,8 @@ export class PrechePage {
       this.audioFile.play();
     }
   }
+
+
 }
+
+//===========================nouvelle methode============================
