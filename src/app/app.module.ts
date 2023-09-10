ead8903cca7c9c42cca578797bb1c8f95f5d0406
ibrahim10sy <ibrahim10sy@gmail.com>
2023-09-10 05:54:26 +0100

    add list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import {  MatDialogModule } from '@angular/material/dialog'
 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -10,10 +11,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListeLectureComponent } from './component/liste-lecture/liste-lecture.component';
 import { SourateComponent } from './component/sourate/sourate.component';
+import { AddListeComponent } from './component/add-liste/add-liste.component';
 
 @NgModule({
-  declarations: [AppComponent,SourateComponent, ListeLectureComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  declarations: [AppComponent,SourateComponent, ListeLectureComponent,AddListeComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,MatDialogModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
