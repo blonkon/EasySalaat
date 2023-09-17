1a249f330e37ba9c5d54d0c974d76e14f061d3bf
Mamadou <mohamedfdg72@gmail.com>
2023-09-17 12:09:26 +0000

    Boussole remontada

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab3',
@@ -8,9 +9,31 @@ import { NavController } from '@ionic/angular';
 })
 export class Tab3Page {
 
-  constructor(private navCtrl: NavController ) {}
+  qiblaDirection?:number;
+
+  constructor(private navCtrl: NavController,private http: HttpClient ) {
+    this.getQiblaDirection(12.6392,7.9266)
+  }
+
+  private apiUrl = 'http://api.aladhan.com/v1/qibla';
+
+
+
+  getQiblaDirection(latitude: number, longitude: number) {
+    const url = `${this.apiUrl}/${latitude}/${longitude}`;
+     this.http.get<any>(url).subscribe(
+      (response) => {
+        this.qiblaDirection=response.data.direction;
+        console.log(response.data.direction)
+      }
+    )
+  }
   Retour() {
     this.navCtrl.back();
   }
+
+
+
+
 
 }
